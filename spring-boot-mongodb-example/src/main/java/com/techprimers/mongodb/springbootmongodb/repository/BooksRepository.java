fee17909e408d8f1c1e00eddf9b7c80983cfa2b8
RoJoSaVs <43160386+RoJoSaVs@users.noreply.github.com>
2019-06-12 20:29:51 -0600

    Delete BooksRepository.java

--- spring-boot-mongodb-example/src/main/java/com/techprimers/mongodb/springbootmongodb/repository/BooksRepository.java
+++
@@ -1,7 +0,0 @@
-package com.techprimers.mongodb.springbootmongodb.repository;
-
-import com.techprimers.mongodb.springbootmongodb.document.Books;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface BooksRepository extends MongoRepository<Books, Integer> {
-}
